b8cbccc07bf6e38150d6b44a58c2cbe0d264a4f4
Steven Oud <soud@protonmail.com>
2016-11-22 12:14:33 +0100

    Add expression test

---
+++ test/mi_parser_test.exs
@@ -0,0 +1,13 @@
+defmodule MiParserTest do
+  alias Mi.{Parser, Lexer, AST}
+  use   ExUnit.Case
+
+  describe "&Parser.parse/1" do
+    test "Expressions are parsed" do
+      {:ok, lexer} = Lexer.lex("(+ 1 (* 5 5))")
+      assert Parser.parse(lexer.tokens) ===
+        [%AST.Expression{operator: :+, lhs: 1,
+                         rhs: %AST.Expression{operator: :*, lhs: 5, rhs: 5}}]
+    end
+  end
+end
